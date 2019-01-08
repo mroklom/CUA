09da018f0560c21805ea82d291726330968a5016
valentin_marechal <valentin.marechal05@gmail.com>
2019-01-08 15:15:34 +0100

    Finished sae and add mlp part

--- SAE/sae_keras.py
+++ SAE/sae_keras.py
@@ -18,40 +18,73 @@ X_test = np.array([np.reshape(matrix / 255.0, 784) for matrix in test_images])
 y_train = to_categorical(train_labels)
 y_test = to_categorical(test_labels)
 
-encoded_dim = 100
+# Hyper Parameters
+# SAE
+encoded_dims = [300, 100, 25]
+sae_epoch = 25
+sae_batch_size = 150
 
+# MLP
+mlp_epoch = 100
+mlp_batch_size = 150
+
+# Declare and compile the models
+# SAE
 input_img = Input(shape=(784,))
-encoded = Dense(encoded_dim, activation='relu')(input_img)
+encoded = Dense(encoded_dims[0], activation='relu')(input_img)
+encoded = Dense(encoded_dims[1], activation='relu')(input_img)
+encoded = Dense(encoded_dims[2], activation='relu')(input_img)
+
+decoded = Dense(encoded_dims[1], activation="relu")(encoded)
+decoded = Dense(encoded_dims[0], activation="relu")(encoded)
 decoded = Dense(784, activation="sigmoid")(encoded)
 
-# this model maps an input to its reconstruction
 autoencoder = Model(input_img, decoded)
-
-# this model maps an input to its encoded representation
 encoder = Model(input_img, encoded)
-
-# create a placeholder for an encoded input
-encoded_input = Input(shape=(encoded_dim,))
-# retrieve the last layer of the autoencoder model
+encoded_input = Input(shape=(encoded_dims[-1],))
 decoder_layer = autoencoder.layers[-1]
-# create the decoder model
 decoder = Model(encoded_input, decoder_layer(encoded_input))
-
 autoencoder.compile(optimizer='adam', loss='mean_squared_error')
 
-autoencoder.fit(X_train, X_train,
-                epochs=5,
-                batch_size=150,
-                shuffle=True,
-                validation_data=(X_test, X_test),
-                verbose=2)
+# MLP
+mlp_layer = Dense(15, activation='relu')(encoded_input)
+mlp_layer = Dense(10, activation='softmax')(mlp_layer)
+mlp = Model(encoded_input, mlp_layer)
+mlp.compile(optimizer='adam', loss='categorical_crossentropy', metrics=["accuracy"])
 
-# encode and decode some digits
-# note that we take them from the *test* set
-encoded_imgs = encoder.predict(X_test)
-decoded_imgs = decoder.predict(encoded_imgs)
+# Fit the models
+# SAE
+autoencoder.fit(
+    x=X_train,
+    y=X_train,
+    epochs=sae_epoch,
+    batch_size=sae_batch_size,
+    shuffle=True,
+    validation_data=(X_test, X_test),
+    verbose=2
+)
 
-n = 10  # how many digits we will display
+# Compute the encoded version of the data
+encoded_images_train = encoder.predict(X_train)
+encoded_images_test = encoder.predict(X_test)
+decoded_images = decoder.predict(encoded_images_test)
+
+# MLP
+mlp.fit(
+    x=encoded_images_train,
+    y=y_train,
+    batch_size=mlp_batch_size,
+    epochs=mlp_epoch,
+    verbose=2,
+    validation_data=(encoded_images_test, y_test)
+)
+
+# Print performances
+score = mlp.evaluate(encoded_images_test, y_test, verbose=0)
+print('Test accuracy:', score[1])
+
+# Print some reconstructed images
+n = 10
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
@@ -63,30 +96,8 @@ for i in range(n):
 
     # display reconstruction
     ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.imshow(decoded_images[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 plt.show()
-
-# model = Sequential()
-# model.add(Dense(784, activation='relu'))
-# model.add(Dense(20, activation='relu'))
-# model.add(Dense(784, activation='relu'))
-#
-# model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error'])
-#
-# model.fit(X_train, X_train, epochs=5, batch_size=150, verbose=2)
-#
-# _, perf = model.evaluate(X_test, X_test, batch_size=150, verbose=2)
-# print("performances on test set : ", perf)
-#
-# # show the reconstruction of the image
-# predictions = model.predict(X_test[:5])
-# f, a = plt.subplots(2, 5)
-# for i, prediction in enumerate(predictions):
-#     reconstructed_image = np.reshape(prediction, (28, 28))
-#     base_image = np.reshape(X_test[i], (28, 28))
-#     a[0][i].imshow(base_image)
-#     a[1][i].imshow(reconstructed_image)
-# plt.show()
